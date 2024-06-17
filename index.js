7a2358c356a68ca309e8e86e92cdaa051573a443
my-coderepository <roshini.pagadala1711@gmail.com>
2024-06-17 16:22:04 +0530

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// App.js or index.js
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        {/* Add more screens as needed */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
